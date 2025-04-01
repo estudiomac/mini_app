a630051f2854bd3dc43c63045914693b0c32b313
evgeniikirillov <es.web.info@gmail.com>
2025-04-01 16:51:08 +0200

    Functions added

--- backend/models/models.py
+++ backend/models/models.py
@@ -21,7 +21,7 @@ class Task(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
     completed: Mapped[bool] = mapped_column(default=False)
-    user: Mapped[int] = mapped_column(ForeignKey("users.id"), ondelete='CASCADE')
+    user: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete='CASCADE'))
 
 async def init_db():
     async with engine.begin() as conn:
